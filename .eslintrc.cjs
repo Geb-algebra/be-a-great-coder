c7b6d28275c6c594a1260508c066999cae127ecb
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2024-05-01 12:52:24 +0900

    remove eslint and update packages

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ["plugin:testing-library/react", "biome"],
-  files: ["app/routes/**/*.test.{ts,tsx}"],
-  rules: {
-    quotes: ["error", "double", { avoidEscape: true }],
-  },
-};
